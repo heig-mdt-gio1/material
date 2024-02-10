8e1227827c00c20ac505a220f96f34eca0a2faf6
OlivierLmr <olivier.lemer@alumni.epfl.ch>
2024-02-10 17:24:39 +0100

    Generated content from master @0eaef0afafd4fd06987998f81171f9929e536544

--- 2024/subject-d1a84d66489622dd4320.js
+++ 2024/subject-d1a84d66489622dd4320.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://tazaf.github.io/heig-mdt-gio1/2022/";
+/******/ 	__webpack_require__.p = "https://heig-mdt-gio1.github.io/material/2024/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 4);
